09a4624db57937d385c920c35257457e46d5a5b0
Bleach <36450388+BleachDrinker420@users.noreply.github.com>
2020-09-11 09:35:58 +0200

    Add the ability to just to /osm instead of /osm _ _ _ _

--- mcosm/src/main/java/bleach/mcosm/OSMCommand.java
+++ mcosm/src/main/java/bleach/mcosm/OSMCommand.java
@@ -1,6 +1,10 @@
 package bleach.mcosm;
 
+import java.math.BigDecimal;
+
 import bleach.mcosm.gui.GuiOSM;
+import bleach.mcosm.utils.GeoPos;
+import net.minecraft.client.Minecraft;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
@@ -17,12 +21,19 @@ public class OSMCommand extends CommandBase {
  
 	@Override
 	public String getUsage(ICommandSender sender) {
-		return "/osm <lat1> <lon1> <lat2> <lon2> | /osm stop";
+		return "/osm | /osm <lat1> <lon1> <lat2> <lon2> | /osm stop";
 	}
 
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
-		if (args.length == 4) {
+		if (args.length == 0) {
+			double[] d = GeoPos.toLatLonBTE(Minecraft.getMinecraft().player.getPosition());
+			McOSM.guiQueue.add(new GuiOSM(
+					new BigDecimal(d[0] - 0.001).setScale(6, BigDecimal.ROUND_HALF_UP).doubleValue(),
+					new BigDecimal(d[1] - 0.001).setScale(6, BigDecimal.ROUND_HALF_UP).doubleValue(),
+					new BigDecimal(d[0] + 0.001).setScale(6, BigDecimal.ROUND_HALF_UP).doubleValue(),
+					new BigDecimal(d[1] + 0.001).setScale(6, BigDecimal.ROUND_HALF_UP).doubleValue()));
+		} else if (args.length == 4) {
 			try {
 				McOSM.guiQueue.add(new GuiOSM(Double.parseDouble(args[0]), Double.parseDouble(args[1]),
 						Double.parseDouble(args[2]), Double.parseDouble(args[3])));
